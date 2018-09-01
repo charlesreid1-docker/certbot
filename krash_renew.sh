4f255bff093630a7e8a45ac0153f7c24920b3bab
Charles Reid <charlesreid1@gmail.com>
2018-09-01 06:06:29 -0700

    add renew script, and wrapper for krash.

---
+++ krash_renew.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# renew/run certbot on krash
+
+set +x 
+
+if [ "$(id -u)" != "0" ]; then
+    echo ""
+    echo ""
+    echo "This script should be run as root."
+    echo ""
+    echo ""
+    exit 1;
+fi
+
+# disable system service that will re-spawn docker pod
+echo "Disable system service:"
+sudo systemctl disable dockerpod-charlesreid1
+sudo systemctl stop dockerpod-charlesreid1
+
+echo "Stop pod:"
+docker-compose -f /home/charles/codes/docker/pod-charlesreid1/docker-compose.yml down
+
+echo "Run certbot renew:"
+sudo ./renew_cert.sh
+
+echo "Start pod:"
+docker-compose -f /home/charles/codes/docker/pod-charlesreid1/docker-compose.yml up -d
+
+echo "Enable system service:"
+sudo systemctl enable dockerpod-charlesreid1
+
+set -x
+
---
+++ renew_cert.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+    echo ""
+    echo ""
+    echo "This script should be run as root."
+    echo ""
+    echo ""
+    exit 1;
+fi
+
+set -x
+
+
+
+##############
+# Domains
+##############
+
+DOMS="charlesreid1.red charlesreid1.blue charlesreid1.com"
+
+for DOM in $DOMS; do
+
+    ## Fake:
+    #echo certbot renew \
+    #    --standalone \
+    #    --non-interactive \
+    #    --agree-tos \
+    #    --email charles@charlesreid1.com \
+    #    -d ${DOM}
+
+    ## Real:
+    #certbot certonly \
+    #    --standalone \
+    #    --non-interactive \
+    #    --agree-tos \
+    #    --email charles@charlesreid1.com \
+    #    -d ${DOM}
+    echo "ohai" 
+
+done
+
+
+
+##############
+# Subdomains
+##############
+
+DOMS="charlesreid1.com"
+SUBS="git files pages hooks bots www"
+
+# Subdomains
+for SUB in $SUBS; do
+    for DOM in $DOMS; do
+
+        ## Fake:
+        #certbot renew \
+        #    --standalone \
+        #    --non-interactive \
+        #    --dry-run \
+        #    --agree-tos \
+        #    --email charles@charlesreid1.com \
+        #    -d ${SUB}.${DOM}
+
+        # Real:
+        certbot certonly \
+            --standalone \
+            --non-interactive \
+            --agree-tos \
+            --email charles@charlesreid1.com \
+            -d ${SUB}.${DOM}
+
+    done
+done
+
+set +x
+
